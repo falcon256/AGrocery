928889047fb3c2e0a274abef4c33fd6dde7c7a28
Falcon256 <256games@gmail.com>
2019-11-01 16:58:59 -0600

    More Gizmo stuff. starting on product based shelf generation.

--- AGrocery/Assets/Dan/StoreGenerator.cs
+++ AGrocery/Assets/Dan/StoreGenerator.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class StoreGenerator : MonoBehaviour
 {
-    public uint numberOfIsles = 2;
+    //public uint numberOfIsles = 2;//depricated
+    public GameObject[] isleAnchors = null;
     public float lengthOfIsles = 50;
     public float widthOfIsles = 10; // the width of the isles from center of shelves
     public Vector3 startLocation = Vector3.zero;
@@ -12,16 +13,19 @@ public class StoreGenerator : MonoBehaviour
     public GameObject shelfGeneratorPrefab = null;
     public GameObject[] shelfPrefabs = null;
     public GameObject[] productPrefabs = null;
-    public ProductRequirement[] requiredProducts = null; // START HERE
     private static StoreGenerator storeGeneratorSingleton = null;
 
     void Start()
     {
         if (storeGeneratorSingleton == null)
             storeGeneratorSingleton = this;
-        for (int isl = 0; isl < numberOfIsles; isl++)
+
+        for(int i = 0; i < isleAnchors.Length; i++)
         {
-            GameObject newIsle = Instantiate(isleGeneratorPrefab, new Vector3(startLocation.x + (widthOfIsles * isl), startLocation.y, startLocation.z), Quaternion.identity);
+            GameObject newIsle = Instantiate(isleGeneratorPrefab, isleAnchors[i].transform.position, isleAnchors[i].transform.rotation);
+            
+            IsleGenerator generator = newIsle.GetComponent<IsleGenerator>();
+            generator.myAnchor = isleAnchors[i].GetComponent<IsleAnchor>();
         }
     }
 
@@ -36,6 +40,22 @@ public class StoreGenerator : MonoBehaviour
         storeGeneratorSingleton = null;     
     }
 
+    public GameObject[] getListOfProductPrefabsByCatagory(ProductData.CATAGORY catagory)
+    {
+        List<GameObject> list = new List<GameObject>();
+        for(int i = 0; i < productPrefabs.Length; i++)
+        {
+            ProductData pd = productPrefabs[i].GetComponent<ProductData>();
+            if(pd==null)
+            {
+                Debug.LogError("A product prefab is missing a product data component.");
+                return null;
+            }
+            if (pd.catagory == catagory)
+                list.Add(productPrefabs[i]);
+        }
+        return list.ToArray();
+    }
     //some have a middle wall with asymetrical occupation.
     //end caps.
 
