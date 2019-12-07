20ad55b89b94f8cef7324267dcc678c9a8247db3
Alex <35349356+alexpeterson2@users.noreply.github.com>
2019-12-06 17:54:53 -0700

    Removed Gazer Pointer Added UIHelpers

--- AGrocery/Assets/Alex/Scripts/VRButton.cs
+++ AGrocery/Assets/Alex/Scripts/VRButton.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VRButton : MonoBehaviour
 {
+    public Image backgroundImg;
+    public Color idleColor;
+    public Color hoverColor;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,15 @@ public class VRButton : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnGazeEnter()
+    {
+        backgroundImg.color = hoverColor;
+    }
+
+    public void OnGazeExit()
+    {
+        backgroundImg.color = idleColor;
     }
 }
