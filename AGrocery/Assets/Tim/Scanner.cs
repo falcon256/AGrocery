a0670e774e3a3ce70ae00a9e04e1a47dec1bc913
TiPaha <timhammes@u.boisestate.edu>
2020-01-30 20:01:01 -0700

    Added Door Activation Script and Animation
    Added Door Activation Script and Animation

---
+++ AGrocery/Assets/Tim/Scanner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scanner : MonoBehaviour
+{
+  private void OnTriggerEnter(Collider other)
+  {
+    Debug.Log("scanner has been triggered");
+  }
+}
