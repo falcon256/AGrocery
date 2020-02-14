3fe633c7d5830867a03b8471ef2616672688ae4c
TiPaha <timhammes@u.boisestate.edu>
2020-02-14 09:30:11 -0700

    Self checkout UI buttons respond on laserpoint click
    Show debug message

---
+++ AGrocery/Assets/Tim/PayScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PayScreen : MonoBehaviour
+{
+  public GameObject cashButton;
+  public GameObject creditButton;
+
+  public bool isCredit;
+
+  public void OnClickCash()
+  {
+    Debug.Log("You have chosen the cash payment method");
+  }
+
+  public void OnClickCredit()
+  {
+    Debug.Log("You have chosen the credit payment method");
+  }
+  
+}
