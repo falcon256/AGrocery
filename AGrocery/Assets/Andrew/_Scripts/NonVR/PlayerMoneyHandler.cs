0df665541c37d9d5a7ac93f5a78b808cfe1daf95
TiPaha <timhammes@u.boisestate.edu>
2020-02-16 09:47:05 -0700

    Menus working with Touch controllers. Show menu with X button.
    Menu elements still need to be positioned and tidied up (ALL)
    Updated scripts with TMPro Text in place of Text and formatted strings for currency "c"

--- AGrocery/Assets/Andrew/_Scripts/NonVR/PlayerMoneyHandler.cs
+++ AGrocery/Assets/Andrew/_Scripts/NonVR/PlayerMoneyHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerMoneyHandler : MonoBehaviour
 {
@@ -47,16 +48,19 @@ public class PlayerMoneyHandler : MonoBehaviour
     public static int Product3Count = 0;
 
     public GameObject playerMoneyTextBox;
-    public Text playerMoneyText;
+    public TextMeshProUGUI playerMoneyText;
 
     public static GameObject product1CountTextBox;
-    public static Text product1CountText;
+    public static TextMeshProUGUI product1CountText;
+
+    public static GameObject currentOfferTextBox;
+    public static TextMeshProUGUI currentOfferText;
 
     public static GameObject product2CountTextBox;
-    public static Text product2CountText;
+    public static TextMeshProUGUI product2CountText;
 
     public static GameObject product3CountTextBox;
-    public static Text product3CountText;
+    public static TextMeshProUGUI product3CountText;
 
     public bool closeToProduct1 = false;
     public bool closeToProduct2 = false;
