3bd211b00d23b7abd91d0f4c302e1f62b563edb4
TimHammes <t.p.hammes@gmail.com>
2020-04-03 11:49:36 -0600

    Imported Scene From Branch (Tim)

--- AGrocery/Assets/Tim/Scripts/PlayerSound.cs
+++ AGrocery/Assets/Tim/Scripts/PlayerSound.cs
@@ -4,88 +4,87 @@ using UnityEngine;
 
 public class PlayerSound : MonoBehaviour
 {
-    AudioSource soundPlayer;
-    GameObject soundObject;
-    SoundManager soundManager;
-    GameObject player;
-    CharacterController characterController;
-    float footstepTimer;
-    public bool isMoving = false;
+  AudioSource soundPlayer;
+  GameObject soundObject;
+  SoundManager soundManager;
+  GameObject player;
+  CharacterController characterController;
+  float footstepTimer;
+  public bool isMoving = false;
 
-    void Start()
+  void Start()
+  {
+    soundObject = GameObject.Find("SoundManager");
+    soundPlayer = transform.GetComponent<AudioSource>();
+    soundManager = soundObject.GetComponent<SoundManager>();
+    characterController = transform.GetComponent<CharacterController>();
+
+  }
+
+  private void FixedUpdate()
+  {
+    //if (characterController.isGrounded == true && characterController.velocity.magnitude > .02f)
+    //{
+    //  isMoving = true;
+    //  if (footstepTimer > .2f)
+    //  {
+
+    //    soundPlayer.volume = .2f;
+    //    soundPlayer.PlayOneShot(soundManager.footsteps);
+
+
+
+    //    footstepTimer = 0;
+    //  }
+
+
+    //  transform.rotation = player.transform.rotation;
+
+
+
+    //}
+    //else
+    //{
+
+
+    //  isMoving = false;
+    //}
+
+    //footstepTimer += Time.deltaTime;
+  }
+
+  void Update()
+  {
+    if (characterController.isGrounded == true && characterController.velocity.magnitude > .02f)
     {
-        soundObject = GameObject.Find("SoundManager");
-        soundPlayer = transform.GetComponent<AudioSource>();
-        soundManager = soundObject.GetComponent<SoundManager>();
-        characterController = transform.GetComponent<CharacterController>();
+      isMoving = true;
+      if (footstepTimer > .5f)
+      {
 
-    }
-
-    private void FixedUpdate()
-    {
-        if (characterController.isGrounded == true && characterController.velocity.magnitude > .02f)
-        {
-            isMoving = true;
-            if (footstepTimer > .2f)
-            {
-
-                soundPlayer.volume = .2f;
-                soundPlayer.PlayOneShot(soundManager.footsteps);
+        soundPlayer.volume = .2f;
+        soundPlayer.PlayOneShot(soundManager.footsteps);
 
 
 
-                footstepTimer = 0;
-            }
-
-
-            transform.rotation = player.transform.rotation;
-
-
-
-        }
-        else
-        {
-
-
-            isMoving = false;
-        }
-
-        footstepTimer += Time.deltaTime;
-    }
-}
-
-//    void Update()
-//  {
-//    if (characterController.isGrounded == true && characterController.velocity.magnitude > .02f)
-//    {
-//      isMoving = true;
-//      if (footstepTimer > .5f)
-//      {
-
-//        soundPlayer.volume = .2f;
-//        soundPlayer.PlayOneShot(soundManager.footsteps);
-
-
-
-//        footstepTimer = 0;
-//      }
+        footstepTimer = 0;
+      }
 
 
       
 
 
 
-//    }
-//    else
-//    {
+    }
+    else
+    {
 
 
-//      isMoving = false;
-//    }
+      isMoving = false;
+    }
 
-//    footstepTimer += Time.deltaTime;
-//  }
+    footstepTimer += Time.deltaTime;
+  }
 
 
-//}
+}
 
--- AGrocery/Assets/Tim/Scripts/ShoppingCart.cs
+++ AGrocery/Assets/Tim/Scripts/ShoppingCart.cs
@@ -5,145 +5,145 @@ using UnityEngine;
 public class ShoppingCart : MonoBehaviour
 {
 
+  
+  GameObject player;
+  AudioSource soundPlayer;
+  GameObject soundObject;
+  SoundManager soundManager;
+  OvrAvatar avatar;
+  GameObject cartHand1;
+  GameObject cartHand2;
+  [SerializeField] GameObject leftHand;
+  [SerializeField] GameObject rightHand;
+ 
+  public float distanceToPlayer;
+  float footstepTimer;
+  public bool isGrabbed;
+  public bool isDisabled;
 
-    GameObject player;
-    AudioSource soundPlayer;
-    GameObject soundObject;
-    SoundManager soundManager;
-    OvrAvatar avatar;
-    GameObject cartHand1;
-    GameObject cartHand2;
-    [SerializeField] GameObject leftHand;
-    [SerializeField] GameObject rightHand;
+  float disabledTimer;
+  float disabledTime = .0f;
 
-    public float distanceToPlayer;
-    float footstepTimer;
-    public bool isGrabbed;
-    public bool isDisabled;
+  void Start()
+  {
+    player = GameObject.Find("OVRPlayerController");
+    soundObject = GameObject.Find("SoundManager");
+    soundPlayer = transform.GetComponent<AudioSource>();
+    soundManager = soundObject.GetComponent<SoundManager>();
+  
+    cartHand1 = new List<GameObject>(GameObject.FindGameObjectsWithTag("CartHand1")).Find(g => g.transform.IsChildOf(this.transform));
+    cartHand2 = new List<GameObject>(GameObject.FindGameObjectsWithTag("CartHand2")).Find(g => g.transform.IsChildOf(this.transform));
 
-    float disabledTimer;
-    float disabledTime = .0f;
+    cartHand1.SetActive(false);
+    cartHand2.SetActive(false);
+    avatar = GameObject.Find("LocalAvatar").GetComponent<OvrAvatar>();
 
-    void Start()
+  
+   
+
+  }
+
+  IEnumerator StopSound()
+  {
+    yield return new WaitForSeconds(soundManager.cartStop.length);
+    soundPlayer.Stop();
+  }
+
+ 
+
+  private void FixedUpdate()
+  {
+    if (player.GetComponent<PlayerSound>().isMoving == true && isGrabbed == true)
     {
-        player = GameObject.Find("OVRPlayerController");
-        soundObject = GameObject.Find("SoundManager");
-        soundPlayer = transform.GetComponent<AudioSource>();
-        soundManager = soundObject.GetComponent<SoundManager>();
 
-        cartHand1 = new List<GameObject>(GameObject.FindGameObjectsWithTag("CartHand1")).Find(g => g.transform.IsChildOf(this.transform));
-        cartHand2 = new List<GameObject>(GameObject.FindGameObjectsWithTag("CartHand2")).Find(g => g.transform.IsChildOf(this.transform));
+      if (footstepTimer > .7f)
+      {
+        soundPlayer.volume = .3f;
+        soundPlayer.PlayOneShot(soundManager.cartWheels);
 
-        cartHand1.SetActive(false);
-        cartHand2.SetActive(false);
-        avatar = GameObject.Find("LocalAvatar").GetComponent<OvrAvatar>();
+
+
+        footstepTimer = 0;
+      }
 
 
 
 
     }
+   
 
-    IEnumerator StopSound()
+    footstepTimer += Time.deltaTime;
+
+  }
+  void Update()
+  {
+
+    distanceToPlayer = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+    GameObject rotateAnchor = GameObject.Find("TrackerAnchor");
+
+    if (OVRInput.GetDown(OVRInput.RawButton.A) == true)
     {
-        yield return new WaitForSeconds(soundManager.cartStop.length);
-        soundPlayer.Stop();
+
+      DisableCart();
+
+    }
+    
+
+    if (isDisabled != true)
+    {
+      if (distanceToPlayer <= 2.2 && (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0 || OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0))
+      {
+        ActivatePushHands(rotateAnchor);
+
+      }
+      else
+      {
+        DeactivatePushHands();
+        
+      }
+    }
+    else
+    {
+      return;
     }
 
 
 
-    private void FixedUpdate()
+  }
+
+  private void DeactivatePushHands()
+  {
+    isGrabbed = false;
+
+    gameObject.transform.parent = null;
+    if (avatar != null)
     {
-        if (player.GetComponent<PlayerSound>().isMoving == true && isGrabbed == true)
-        {
-
-            if (footstepTimer > .7f)
-            {
-                soundPlayer.volume = .3f;
-                soundPlayer.PlayOneShot(soundManager.cartWheels);
-
-
-
-                footstepTimer = 0;
-            }
-
-
-
-
-        }
-
-
-        footstepTimer += Time.deltaTime;
-
-    }
-    void Update()
-    {
-
-        distanceToPlayer = Vector3.Distance(gameObject.transform.position, player.transform.position);
-
-        GameObject rotateAnchor = GameObject.Find("TrackerAnchor");
-
-        if (OVRInput.GetDown(OVRInput.RawButton.A) == true)
-        {
-
-            DisableCart();
-
-        }
-
-
-        if (isDisabled != true)
-        {
-            if (distanceToPlayer <= 2.2 && (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0 || OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0))
-            {
-                ActivatePushHands(rotateAnchor);
-
-            }
-            else
-            {
-                DeactivatePushHands();
-
-            }
-        }
-        else
-        {
-            return;
-        }
-
-
-
+      avatar.ShowFirstPerson = true;
     }
 
-    private void DeactivatePushHands()
+    cartHand1.SetActive(false);
+    cartHand2.SetActive(false);
+  }
+
+  private void ActivatePushHands(GameObject rotateAnchor)
+  {
+    isGrabbed = true;
+
+    if (isGrabbed)
     {
-        isGrabbed = false;
+      avatar.ShowFirstPerson = false;
+      cartHand1.SetActive(true);
+      cartHand2.SetActive(true);
 
-        gameObject.transform.parent = null;
-        if (avatar != null)
-        {
-            avatar.ShowFirstPerson = true;
-        }
 
-        cartHand1.SetActive(false);
-        cartHand2.SetActive(false);
+      gameObject.transform.parent = rotateAnchor.transform;
     }
+  }
 
-    private void ActivatePushHands(GameObject rotateAnchor)
-    {
-        isGrabbed = true;
+  private void DisableCart()
+  {
+      isDisabled = !isDisabled;
 
-        if (isGrabbed)
-        {
-            avatar.ShowFirstPerson = false;
-            cartHand1.SetActive(true);
-            cartHand2.SetActive(true);
-
-
-            gameObject.transform.parent = rotateAnchor.transform;
-        }
-    }
-
-    private void DisableCart()
-    {
-        isDisabled = !isDisabled;
-
-    }
+  }
 }
