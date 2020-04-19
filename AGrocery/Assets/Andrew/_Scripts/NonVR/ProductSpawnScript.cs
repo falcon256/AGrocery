62db9bb82ebb66570428b90636dea98022c6259a
TimHammes <t.p.hammes@gmail.com>
2020-04-19 12:23:55 -0600

    Added more labels(text). Started stocking bread aisle. Modified Product Spawn script to spawn to parent.

--- AGrocery/Assets/Andrew/_Scripts/NonVR/ProductSpawnScript.cs
+++ AGrocery/Assets/Andrew/_Scripts/NonVR/ProductSpawnScript.cs
@@ -5,51 +5,54 @@ using UnityEngine.UI;
 
 public class ProductSpawnScript : MonoBehaviour
 {
-    public GameObject player;
+  public GameObject player;
 
-    public GameObject product1;
-    public GameObject product2;
-    public GameObject product3;
+  public GameObject product1;
+  public GameObject product2;
+  public GameObject product3;
 
-   /* public GameObject textBox;
-    public Text text;
-    */
+  /* public GameObject textBox;
+   public Text text;
+   */
 
-    //public int count = 0;
+  //public int count = 0;
 
-    public bool closeToPlayer;
+  public bool closeToPlayer;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    player = GameObject.FindWithTag("Player");
+
+    //textBox = GameObject.FindWithTag("NotificationText");
+
+    spawnRooms();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+
+  public void spawnRooms()
+  {
+    var productToSpawn = Random.Range(0, 3);
+
+    if (productToSpawn == 0)
     {
-        player = GameObject.FindWithTag("Player");
-
-        //textBox = GameObject.FindWithTag("NotificationText");
-
-        spawnRooms();     
+      var product1Spawned = (GameObject)Instantiate(product1, transform.position, transform.rotation);
+      product1Spawned.transform.parent = transform;
     }
-
-    // Update is called once per frame
-    void Update()
-        {
-            }    
-
-    public void spawnRooms()
+    if (productToSpawn == 1)
     {
-        var productToSpawn = Random.Range(0, 3);
-
-        if (productToSpawn == 0)
-        {
-            var product1Spawned = (GameObject)Instantiate(product1, transform.position, transform.rotation);
-        }
-        if (productToSpawn == 1)
-        {
-            var product2Spawned = (GameObject)Instantiate(product2, transform.position, transform.rotation);
-        }
-        if (productToSpawn == 2)
-        {
-            var product3Spawned = (GameObject)Instantiate(product3, transform.position, transform.rotation);
-        }
+      var product2Spawned = (GameObject)Instantiate(product2, transform.position, transform.rotation);
+      product2Spawned.transform.parent = transform;
     }
-
+    if (productToSpawn == 2)
+    {
+      var product3Spawned = (GameObject)Instantiate(product3, transform.position, transform.rotation);
+      product3Spawned.transform.parent = transform;
     }
+  }
+
+}
--- AGrocery/Assets/Dan/PhysicsCollisionTrigger.cs
+++ AGrocery/Assets/Dan/PhysicsCollisionTrigger.cs
@@ -2,39 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PhysicsCollisionTrigger : MonoBehaviour
 {
-    private GameObject firstCollider = null;
-    private Rigidbody myRB = null;
-    public void OnCollisionEnter(Collision collision)
-    {
+  private GameObject firstCollider = null;
+  private Rigidbody myRB = null;
+  public void OnCollisionEnter(Collision collision)
+  {
 
-        if (this.gameObject.GetComponent<Rigidbody>() == null && collision.gameObject == firstCollider)
-            return;
-        if (this.gameObject.GetComponent<Rigidbody>())
+    if (this.gameObject.GetComponent<Rigidbody>() == null && collision.gameObject == firstCollider)
+      return;
+    if (this.gameObject.GetComponent<Rigidbody>())
+    {
+      if (!firstCollider)
+      {
+        firstCollider = collision.gameObject;
+        myRB = this.gameObject.GetComponent<Rigidbody>();
+        if (!myRB)
         {
-            if (!firstCollider)
-            {
-                firstCollider = collision.gameObject;
-                myRB = this.gameObject.GetComponent<Rigidbody>();
-                if (!myRB)
-                {
-                    Debug.LogError("There is no Rigib Body on this object, but you added Dan's RB disabler onto it...");
-                }
-                else
-                {
-                    Destroy(myRB);
-                    myRB = null;
-                }
-            }
+          Debug.LogError("There is no Rigib Body on this object, but you added Dan's RB disabler onto it...");
         }
         else
         {
-            myRB = this.gameObject.AddComponent<Rigidbody>();
-            myRB.mass = 0.1f;
-            myRB.isKinematic = false;
-            myRB.useGravity = true;
-            return;
+          Destroy(myRB);
+          myRB = null;
         }
+      }
     }
+    else
+    {
+      setupRB();
+      return;
+    }
+  }
+
+  public void setupRB()
+  {
+    if (myRB)
+      return;
+
+    myRB = this.gameObject.AddComponent<Rigidbody>();
+    myRB.mass = 0.1f;
+    myRB.isKinematic = false;
+    myRB.useGravity = true;
+    return;
+  }
 }
